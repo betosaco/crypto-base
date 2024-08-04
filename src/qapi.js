cc5019038d284f9917c438e24e9b68f9d26ee52a
Labet Saconi <63941081+betosaco@users.noreply.github.com>
2024-08-03 22:18:44 -0500

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"66aef2f9995d120018f2a8ba",
+				"66aef2f9995d120018f2a8bd"
+			]
+		},
+		"66aef2f9995d120018f2a8ba": {
+			"id": "66aef2f9995d120018f2a8ba",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"66aef2f9995d120018f2a8bd": {
+			"id": "66aef2f9995d120018f2a8bd",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
